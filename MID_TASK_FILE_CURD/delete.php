24eecf5c7916d49a287e1514bf7298224dce4f01
mohaimenurrahman <mohaimenur888@gmail.com>
2021-10-16 12:49:52 +0600

    into to curd

--- MID_TASK_FILE_CURD/delete.php
+++ MID_TASK_FILE_CURD/delete.php
@@ -1,5 +1,27 @@
-<?php 
+<?php
+$id = $_GET['id'];
+if (copy('user.txt', 'userBac.txt') != 1) {
+	echo 'Error backing up';
+	return;
+}
+$myBacfile = fopen('userBac.txt', 'r');
+$myfile = fopen('user.txt', 'w');
+$counter = 0;
 
-	echo $_GET['id'];
+while (!feof($myBacfile)) {
+	$data = fgets($myBacfile);
+	if ($data != "") {
+		if (++$counter != $id) {
+			fwrite($myfile, $data);
+		}
+	}
+}
 
+header('location: userlist.php');
+
+fclose($myfile);
+fclose($myBacfile);
+$myBacfile = fopen('userBac.txt', 'w');
+fwrite($myBacfile, "");
+fclose($myBacfile);
 ?>
